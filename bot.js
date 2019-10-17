8b36d3a7417aff165f20bfa014fd18f843a293d4
niaz151 <niaz151@gmail.com>
2019-10-17 17:35:41 -0400

    Added GIF API

--- bot.js
+++ bot.js
@@ -3,6 +3,10 @@ const auth = require('./auth.json');
 const request = require('request');
 const client = new Discord.Client();
 
+
+  
+/* ##########  GLOBAL VARS ##########  */
+  
 var ganks_top = 0
 var ganks_mid = 0
 var ganks_bot = 0
@@ -14,20 +18,22 @@ client.on('ready', () => {
 
 
 client.on('message', msg => {
-  if(msg.content === '!die-roll') {
+
+  const args = msg.content.slice(1).split(' ');
+  const command = args.shift().toLowerCase();
+  
+  /* ########## DIE ROLL ##########  */
+
+  if(command === 'die-roll') {
     var number = Math.round(Math.random() * 6)
     msg.reply(number)
   }
-})
 
-
-client.on('message', msg => {
+  /* ########## JOKE ##########  */
   
-  const args = msg.content.slice(1).split(' ');
-  const command = args.shift().toLowerCase();
-  const endpoint = 'https://icanhazdadjoke.com/'
-
   if (command === 'joke'){
+
+    const endpoint = 'https://icanhazdadjoke.com/'
 
     var options = {
       url: endpoint,
@@ -38,12 +44,37 @@ client.on('message', msg => {
 
     function callback(error,response,body){
       info = JSON.parse(body)
-      console.log(info.joke)
       msg.reply(info.joke)
     }
 
     request(options,callback)
   }
+
+  /* ########## GIFS ##########  */
+  
+  if(command === 'gif'){
+
+    if(args.length === 0){
+      msg.reply("Missing search query")
+    }
+
+    const base = "http://api.giphy.com/v1/gifs/search?"
+    var query = args.join("+") 
+    var api_key = auth.gif_token 
+    var limit = 30
+    var endpoint =  base+"q="+query+"&"+"api_key="+api_key+"&"+"limit="+limit
+
+    var index = Math.round(Math.random() * limit)
+
+    request(endpoint, (error,response,body) => {
+      var info = JSON.parse(body)
+      var img = info.data[index].images.original.url
+      msg.channel.send(img)
+    })
+  }
+
+
+
 })
 
-client.login(auth.token)
+client.login(auth.bot_token)
